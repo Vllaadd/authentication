bf96f2befd24809d9cb94f8ad180692ef7a039b1
Vllaadd <vlladdiimiir@gmail.com>
2023-05-10 05:27:37 -0400

    delete function

--- auth/auth.js
+++ auth/auth.js
@@ -75,4 +75,14 @@ exports.update = async (req, res, next) => {
 };
 
 
-
+//delete function 
+exports.deleteUser = async(req, res, next) => {
+  const { id } = req.body
+  await User.findById(id)
+    .then(user => user.remove())
+    .then(user => 
+        res.status(201).json({message: "User successfully deleted", user})
+        )
+        .catch(error => 
+          res.status(400).json({ message: "An error occurred", error: error.message }))
+}
--- auth/route.js
+++ auth/route.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { register, login, update } = require("./auth");
+const { register, login, update, deleteUser } = require("./auth");
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/update").put(update)
+router.route("/update").put(update);
+router.route("/deleteUser").delete(deleteUser);
 
 
 

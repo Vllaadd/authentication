7457a534b3a2974fbbd44fba5186a219928aa3cd
Vllaadd <vlladdiimiir@gmail.com>
2023-05-11 01:08:22 -0400

    type in bcryptjs

--- auth/auth.js
+++ auth/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 
 //registration function 
 exports.register = async (req, res, next) => {

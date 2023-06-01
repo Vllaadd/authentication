0e24770308ce52b068fce3817fcf278c50813e9b
Vllaadd <vlladdiimiir@gmail.com>
2023-06-01 02:43:57 -0400

    jwt not recognized

--- auth/auth.js
+++ auth/auth.js
@@ -8,12 +8,12 @@ exports.register = async (req, res, next) => {
     const { username, password } = req.body
     if(password.length < 6) {
         return res.status(400).json({ message: "Password less than 6 characters"})
-    }try{
-     await bcryptjs.hash(password, 10).then(async (hash) => {
+    }
+     bcryptjs.hash(password, 10).then(async (hash) => {
         await User.create({
             username, 
             password: hash,
-        }).then(user => {
+        }).then((user) => {
           const maxAge = 3 * 60 * 60;
           const token = jwt.sign(
             { id: user._id, username, role: user.role },
@@ -26,9 +26,10 @@ exports.register = async (req, res, next) => {
             httpOnly: true,
             maxAge: maxAge * 1000, // 3hrs in ms
           });
-            res.status(200).json({
+            res.status(201).json({
                 message: "User successfully created",
-                user,
+                user: user._id,
+                role: user.role,
             })
           })
     .catch((error) =>
@@ -38,13 +39,7 @@ exports.register = async (req, res, next) => {
         })
       );
      });
-    } catch (error) {
-      res.status(400).json({
-        message: "An error occurred",
-        error: error.message,
-      });
-    };
-  };
+    } 
 
   exports.getUsers = async (req, res, next) => {
     await User.find({})

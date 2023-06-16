36f05163e5217671eb9269f9d9ca5ca24859e628
Vllaadd <vlladdiimiir@gmail.com>
2023-06-16 05:43:10 -0400

    comment change

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 // ROUTES
-
 app.get("/", (req, res) => res.render("home"));
 app.get("/register", (req, res) => res.render("register"));
 app.get("/login", (req, res) => res.render("login"));
